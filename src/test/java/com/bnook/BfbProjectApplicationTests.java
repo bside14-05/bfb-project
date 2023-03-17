2ba1ac38193d018edbf0e8c05e05d5819a245e8c
jinLee <etoileettoi@gmail.com>
2023-03-18 03:05:03 +0900

    프로젝트 세팅

---
+++ src/test/java/com/bnook/BfbProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bnook;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class BfbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
