3c8910a753ba6edb503781abdd5baf3ae2b38923
jkjkh1318 <jkjkh1318@gmail.com>
2023-04-29 23:56:43 +0900

    bookstore entity 칼럼명 수정

--- src/main/java/com/bnook/v1/domain/bookstore/Bookstore.java
+++ src/main/java/com/bnook/v1/domain/bookstore/Bookstore.java
@@ -14,114 +14,112 @@ import org.hibernate.annotations.DynamicUpdate;
 public class Bookstore extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "STORE_ID")
-    private Long storeId;
+    @Column(name = "bookstore_id")
+    private Long bookstoreId;
 
-    @Column(nullable = false, name = "STORE_NAME")
-    private String storeName;
+    @Column(nullable = false, name = "bookstore_name")
+    private String bookstoreName;
 
-    @Column(name = "OWNER_ONE_WORD")
-    private String ownerOneWord;
-
-//    private String ownerName;
+    @Column(name = "owner_comment")
+    private String ownerComment;
 
     private String address;
 
-    @Column(name = "PHONE_NO")
-    private String phoneNo;
+    @Column(name = "phone_number")
+    private String phoneNumber;
 
-    @Column(name = "INSTAGRAM_ID")
+    @Column(name = "instagram_id")
     private String instagramId;
 
-    @Column(name = "FACEBOOK_ID")
+    @Column(name = "facebook_id")
     private String facebookId;
 
-    @Column(name = "BLOG_ID")
+    @Column(name = "blog_id")
     private String blogId;
 
     private String website;
 
-    @Column(name = "BUSINESS_DAYS")
-    private String businessDays;
+    @Column(name = "program_category1")
+    private String programCategory1;
 
-    @Column(name = "CLOSE_DAYS")
-    private String closeDays;
+    @Column(name = "program_category2")
+    private String programCategory2;
 
-    @Column(name = "PRGR_CTGR1")
-    private String prgrCtgr1;
+    @Column(name = "program_category3")
+    private String programCategory3;
 
-    @Column(name = "PRGR_CTGR2")
-    private String prgrCtgr2;
+    @Column(name = "is_beverage_alcohol")
+    private boolean isBeverageAlcohol;
 
-    @Column(name = "PRGR_CTGR3")
-    private String prgrCtgr3;
+    @Column(name = "is_art_exhibition")
+    private boolean isArtExhibition;
 
-    @Column(name = "BEVERAGE_YN_CTGR")
-    private String beverageYnCtgr;
+    @Column(name = "is_merchandise_sale")
+    private boolean isMerchandiseSale;
 
-    @Column(name = "ART_YN_CTGR")
-    private String artYnCtgr;
+    @Column(name = "is_program_operating")
+    private boolean isProgramOperating;
 
-    @Column(name = "PRODUCT_YN_CTGR")
-    private String productYnCtgr;
+    @Column(name = "is_other")
+    private boolean isOther;
 
-    @Column(name = "INDPN_BOOK_FILTER")
-    private String indpnBookFilter;
+    @Column(name = "has_indie_book")
+    private boolean hasIndieBook;
 
-    @Column(name = "SPCL_BOOK_FILTER")
-    private String spclBookFilter;
+    @Column(name = "has_specialized_book")
+    private boolean hasSpecializedBook;
 
-    @Column(name = "CMPRN_BOOK_FILTER")
-    private String cmprnBookFilter;
+    @Column(name = "has_comprehensive_book")
+    private boolean hasComprehensiveBook;
 
-    @Column(name = "FORGN_BOOK_FILTER")
-    private String forgnBookFilter;
+    @Column(name = "has_foreign_book")
+    private boolean hasForeignBook;
 
     @Builder
-    public Bookstore(String storeName, String ownerOneWord, String address, String phoneNo, String instagramId, String facebookId, String blogId, String website, String businessDays, String closeDays, String prgrCtgr1, String prgrCtgr2, String prgrCtgr3, String beverageYnCtgr, String artYnCtgr, String productYnCtgr, String indpnBookFilter, String spclBookFilter, String cmprnBookFilter, String forgnBookFilter) {
-        this.storeName = storeName;
-        this.ownerOneWord = ownerOneWord;
+    public Bookstore(String bookstoreName, String ownerComment, String address, String phoneNumber, String instagramId, String facebookId, String blogId, String website, String programCategory1, String programCategory2, String programCategory3, boolean isBeverageAlcohol, boolean isArtExhibition, boolean isMerchandiseSale, boolean isProgramOperating, boolean isOther, boolean hasIndieBook, boolean hasSpecializedBook, boolean hasComprehensiveBook, boolean hasForeignBook) {
+        this.bookstoreName = bookstoreName;
+        this.ownerComment = ownerComment;
         this.address = address;
-        this.phoneNo = phoneNo;
+        this.phoneNumber = phoneNumber;
         this.instagramId = instagramId;
         this.facebookId = facebookId;
         this.blogId = blogId;
         this.website = website;
-        this.businessDays = businessDays;
-        this.closeDays = closeDays;
-        this.prgrCtgr1 = prgrCtgr1;
-        this.prgrCtgr2 = prgrCtgr2;
-        this.prgrCtgr3 = prgrCtgr3;
-        this.beverageYnCtgr = beverageYnCtgr;
-        this.artYnCtgr = artYnCtgr;
-        this.productYnCtgr = productYnCtgr;
-        this.indpnBookFilter = indpnBookFilter;
-        this.spclBookFilter = spclBookFilter;
-        this.cmprnBookFilter = cmprnBookFilter;
-        this.forgnBookFilter = forgnBookFilter;
+        this.programCategory1 = programCategory1;
+        this.programCategory2 = programCategory2;
+        this.programCategory3 = programCategory3;
+        this.isBeverageAlcohol = isBeverageAlcohol;
+        this.isArtExhibition = isArtExhibition;
+        this.isMerchandiseSale = isMerchandiseSale;
+        this.isProgramOperating = isProgramOperating;
+        this.isOther = isOther;
+        this.hasIndieBook = hasIndieBook;
+        this.hasSpecializedBook = hasSpecializedBook;
+        this.hasComprehensiveBook = hasComprehensiveBook;
+        this.hasForeignBook = hasForeignBook;
     }
 
-    public void update(String storeName, String ownerOneWord, String address, String phoneNo, String instagramId, String facebookId, String blogId, String website, String businessDays, String closeDays, String prgrCtgr1, String prgrCtgr2, String prgrCtgr3, String beverageYnCtgr, String artYnCtgr, String productYnCtgr, String indpnBookFilter, String spclBookFilter, String cmprnBookFilter, String forgnBookFilter) {
-        this.storeName = storeName;
-        this.ownerOneWord = ownerOneWord;
+    public void update(String bookstoreName, String ownerComment, String address, String phoneNumber, String instagramId, String facebookId, String blogId, String website, String programCategory1, String programCategory2, String programCategory3, boolean isBeverageAlcohol, boolean isArtExhibition, boolean isMerchandiseSale, boolean isProgramOperating, boolean isOther, boolean hasIndieBook, boolean hasSpecializedBook, boolean hasComprehensiveBook, boolean hasForeignBook) {
+        this.bookstoreName = bookstoreName;
+        this.ownerComment = ownerComment;
         this.address = address;
-        this.phoneNo = phoneNo;
+        this.phoneNumber = phoneNumber;
         this.instagramId = instagramId;
         this.facebookId = facebookId;
         this.blogId = blogId;
         this.website = website;
-        this.businessDays = businessDays;
-        this.closeDays = closeDays;
-        this.prgrCtgr1 = prgrCtgr1;
-        this.prgrCtgr2 = prgrCtgr2;
-        this.prgrCtgr3 = prgrCtgr3;
-        this.beverageYnCtgr = beverageYnCtgr;
-        this.artYnCtgr = artYnCtgr;
-        this.productYnCtgr = productYnCtgr;
-        this.indpnBookFilter = indpnBookFilter;
-        this.spclBookFilter = spclBookFilter;
-        this.cmprnBookFilter = cmprnBookFilter;
-        this.forgnBookFilter = forgnBookFilter;
+        this.programCategory1 = programCategory1;
+        this.programCategory2 = programCategory2;
+        this.programCategory3 = programCategory3;
+        this.isBeverageAlcohol = isBeverageAlcohol;
+        this.isArtExhibition = isArtExhibition;
+        this.isMerchandiseSale = isMerchandiseSale;
+        this.isProgramOperating = isProgramOperating;
+        this.isOther = isOther;
+        this.hasIndieBook = hasIndieBook;
+        this.hasSpecializedBook = hasSpecializedBook;
+        this.hasComprehensiveBook = hasComprehensiveBook;
+        this.hasForeignBook = hasForeignBook;
     }
 }
 
